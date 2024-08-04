3482b221de50efa0a56f8779133d4680320f1529
Denex A Poulose <107526656+denex-a-poulose@users.noreply.github.com>
2024-08-04 13:10:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dark: '#1a1a1a',
+        blue:'#729fcf',
+        violet:'#a180e8',
+        red:'#cc0000',
+        green:'#4e9a06'
+      }
+    }
+  }
+}
